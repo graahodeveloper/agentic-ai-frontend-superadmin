4a099fc7be17b32a1a0faf8cae1db37fbd49ef6a
GOLAM KIBRIA ANIK <golamkibriaanik@GOLAMs-Mac-mini.local>
2026-04-12 14:52:03 +0600

    added the cms setting

---
+++ src/app/dashboard/cms-settings/login-page/page.tsx
@@ -0,0 +1,7 @@
+'use client';
+
+import LoginPageSettings from '@/components/cms-settings/LoginPageSettings';
+
+export default function LoginPageSettingsPage() {
+  return <LoginPageSettings />;
+}
